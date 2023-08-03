7148f5e090d49929078137fcd35550e22e1a742f
Anilsh007 <anil.sh101@gmail.com>
2023-08-02 21:18:29 -0700

    push

--- mail.php
+++ mail.php
@@ -1,28 +1,23 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Get form data
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $subject = $_POST["subject"];
-    $message = $_POST["message"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Get form data
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $subject = $_POST["subject"];
+  $message = $_POST["message"];
 
-    // Validate form data (you can add more validation as needed)
-    if (empty($name) || empty($email) || empty($subject) || empty($message)) {
-        echo "All fields are required.";
-        exit;
-    }
+  // Set up email headers
+  $to = "anilsconcept@gmail.com"; // Change this to the recipient's email address
+  $headers = "From: $name <$email>\r\n";
+  $headers .= "Reply-To: $email\r\n";
+  
+  // Send the email
+  $mailSent = mail($to, $subject, $message, $headers);
 
-    // Set up email parameters
-    $to = "anilsconcept@gmail.com"; // Replace with the recipient's email address
-    $headers = "From: $name <$email>\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/html\r\n";
-
-    // Send the email
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Email sent successfully!";
-    } else {
-        echo "Failed to send the email. Please try again later.";
-    }
+  if ($mailSent) {
+    echo "Email sent successfully!";
+  } else {
+    echo "Failed to send the email.";
+  }
 }
 ?>
